a4a78cc3704098f9006d9d6095bff9354ce18d08
olpex <olpex@ukr.net>
2024-11-10 11:53:42 +0200

    add tojs_1

--- scripts.js
+++ scripts.js
@@ -2,3 +2,7 @@ const text = document.getElementsByTagName("p");
 for (let i = 0; i < text.length; i++) {
   text[i].style.color = "red";
 }
+
+const textElement = getElementsByTagName("p");
+const textContent = textElement.innerHTML();
+console.log(textContent);
